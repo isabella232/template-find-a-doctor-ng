9f0de369881e1bbeb7cfc1ecfd483fcfa189cb39
sis0k0 <stanimira.vlaeva@gmail.com>
2018-08-27 16:35:59 +0300

    refactor: resolve lazy modules with absolute path

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -6,13 +6,13 @@ import { LoggedInLazyLoadGuard } from "./logged-in-lazy-load.guard";
 
 const routes: Routes = [
     { path: "", redirectTo: "/root", pathMatch: "full" },
-    { path: "root", loadChildren: "./root/root.module#RootModule", canLoad: [LoggedInLazyLoadGuard] },
-    { path: "calculator", loadChildren: "./calculator/calculator.module#CalculatorModule", canLoad: [LoggedInLazyLoadGuard] },
-    { path: "search", loadChildren: "./search/search.module#SearchModule", canLoad: [LoggedInLazyLoadGuard] },
-    { path: "plan", loadChildren: "./plan/plan.module#PlanModule", canLoad: [LoggedInLazyLoadGuard] },
-    { path: "results", loadChildren: "./results/results.module#ResultsModule", canLoad: [LoggedInLazyLoadGuard] },
-    { path: "calculator-result", loadChildren: "./calculator-result/calculator-result.module#CalculatorResultModule", canLoad: [LoggedInLazyLoadGuard] },
-    { path: "login", loadChildren: "./login/login.module#LoginModule" }
+    { path: "root", loadChildren: "~/app/root/root.module#RootModule", canLoad: [LoggedInLazyLoadGuard] },
+    { path: "calculator", loadChildren: "~/app/calculator/calculator.module#CalculatorModule", canLoad: [LoggedInLazyLoadGuard] },
+    { path: "search", loadChildren: "~/app/search/search.module#SearchModule", canLoad: [LoggedInLazyLoadGuard] },
+    { path: "plan", loadChildren: "~/app/plan/plan.module#PlanModule", canLoad: [LoggedInLazyLoadGuard] },
+    { path: "results", loadChildren: "~/app/results/results.module#ResultsModule", canLoad: [LoggedInLazyLoadGuard] },
+    { path: "calculator-result", loadChildren: "~/app/calculator-result/calculator-result.module#CalculatorResultModule", canLoad: [LoggedInLazyLoadGuard] },
+    { path: "login", loadChildren: "~/app/login/login.module#LoginModule" }
 ];
 
 @NgModule({
